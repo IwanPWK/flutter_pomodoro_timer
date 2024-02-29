eb60661f1a35da0308d0b771b97b5a31e504bf81
IwanPWK <koadhyow@gmail.com>
2024-02-29 11:43:40 +0700

    Pomodoro screen

---
+++ lib/pomodoro_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'utils.dart';
+
+class PomodoroScreen extends StatelessWidget {
+  const PomodoroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.redAccent,
+        title: Text(
+          "POMODORO",
+          style: textStyle(25, Colors.white, FontWeight.w700),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.refresh,
+              color: Colors.white,
+            ),
+            iconSize: 40,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+          child: Container(
+              alignment: Alignment.center,
+              child: const Column(children: [
+                SizedBox(
+                  height: 15,
+                )
+              ]))),
+    );
+  }
+}
